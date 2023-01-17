314b91d3504aea3b0872e7885d8877f4ec0b37e7
akhfirdaus07 <121778146+akhfirdaus07@users.noreply.github.com>
2023-01-17 20:46:10 +0700

    Add removeEventListner in buttons

--- javascript.js
+++ javascript.js
@@ -1,5 +1,5 @@
+// Make Grid
 const grid = document.querySelector(".grid");
-
 function makeGrid(rows, cols) {
     grid.style.setProperty('--grid-rows', rows);
     grid.style.setProperty('--grid-cols', cols);
@@ -8,64 +8,66 @@ function makeGrid(rows, cols) {
         grid.appendChild(cell).className = "grid-item";
     };
 };
-
 makeGrid(64, 64);
 
 const gridItems=document.querySelectorAll(".grid-item");
 
-const blackButton=document.querySelector(".black-ink");
+const rainbowButton=document.querySelector(".rainbow-ink");
 
-blackButton.addEventListener("click", addMouseOver)
+
+// Styling the button when clicked
+const buttons=document.querySelectorAll(".button");
+for(const button of buttons){
+    button.addEventListener("click", toggleActive)
+}
+function toggleActive(){
+    clearActive();
+    this.classList.add("active");
+}
+function clearActive(){
+    for(const button of buttons){
+        button.classList.remove("active");
+    }
+};
+
+
+
+
+// Black button function
+const blackButton=document.querySelector(".black-ink");
+blackButton.addEventListener("click", addMouseOver);
 
 function addMouseOver(){
     for (let gridItem of gridItems){
         gridItem.addEventListener("mouseover", gridBlack);
+        gridItem.removeEventListener("mouseover", gridErase);
     }
 }
-
 function gridBlack() {
     this.classList.add("grid-black");
 }
 
 
-
-const clearButton=document.querySelector(".clear");
-
-clearButton.addEventListener("click", clearGrid)
-
-function clearGrid(){
-    for(let gridItem of gridItems){
-        gridItem.classList.remove("grid-black")
-    }
-}
-
+// Eraser button function
 const eraserButton=document.querySelector(".eraser");
-
 eraserButton.addEventListener("click", eraser);
 
 function eraser(){
     for (let gridItem of gridItems){
         gridItem.addEventListener("mouseover", gridErase)
+        gridItem.removeEventListener("mouseover", gridBlack)
     }
 }
-
 function gridErase() {
     this.classList.remove("grid-black")   
 }
 
-const buttons=document.querySelectorAll(".button");
-
-for(const button of buttons){
-    button.addEventListener("click", toggleActive)
-}
-
-function toggleActive(){
-    clearActive();
-    this.classList.add("active");
-}
-
-function clearActive(){
-    for(const button of buttons){
-        button.classList.remove("active");
+// Clear button function
+const clearButton=document.querySelector(".clear");
+clearButton.addEventListener("click", clearGrid)
+function clearGrid(){
+    for(let gridItem of gridItems){
+        gridItem.removeEventListener("mouseover", gridBlack);
+        gridItem.classList.remove("grid-black");
     }
-}
+};
